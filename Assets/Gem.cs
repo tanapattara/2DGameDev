bfcdabd9e689e2413ff217e08402663c4eba352d
Tanapattara Wongkhamchan <tanapattara.w@gmail.com>
2024-09-05 19:03:37 +0700

    Gem and update ui

--- Assets/Gem.cs
+++ Assets/Gem.cs
@@ -10,9 +10,11 @@ public class Gem : MonoBehaviour
         
     }
     private void OnTriggerEnter2D(Collider2D collision)
-    {
+    { 
         if(collision.CompareTag("Player"))
         {
+            collision.GetComponent<PlayCollectibles>().GemCollected();
+
             GetComponent<AudioSource>().Play();
             GetComponent<SpriteRenderer>().enabled = false;
             GetComponent<Collider2D>().enabled = false;
---
+++ Assets/PlayCollectibles.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class PlayCollectibles : MonoBehaviour
+{
+    public Text textComponent;
+    public int gemNumber = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateText();
+        
+    }
+
+    private void UpdateText() 
+    {
+        textComponent.text = gemNumber.ToString();
+    }
+    public void GemCollected() 
+    { 
+        gemNumber++;
+        UpdateText();
+    }
+}
